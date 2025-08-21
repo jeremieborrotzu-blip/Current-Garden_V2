91b5f692d0b703ac7e9397fe2d26e7481c4cc401
maxime-rancon-oc <maxime.rancon@openclassrooms.com>
2025-08-21 11:19:59 +0200

    bug fixes

--- data/diay0794_jardinactuel.sql
+++ data/diay0794_jardinactuel.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `current_garden`
+-- Database: `jardin_actuel`
 --
 
 -- --------------------------------------------------------
